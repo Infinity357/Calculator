05e72cb84527d1556da5a8d70ad6d7c39656a221
unknown <aviralgit0384@gmail.com>
2024-08-08 18:19:32 +0530

    final fourth

--- app/src/main/java/com/example/simplecalculator/calculatorUI.kt
+++ app/src/main/java/com/example/simplecalculator/calculatorUI.kt
@@ -1,6 +1,7 @@
 package com.example.simplecalculator
 
 import android.annotation.SuppressLint
+import android.content.res.Configuration
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -21,6 +22,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -59,6 +61,23 @@ fun CalculatorUI(
     state: State<String>,
     onAction: (CalculatorAction)->Unit
 ){
+
+    var textPadding : Dp = 15.dp
+    var DisplayWeight = 7f
+    var ButtonText = 30.sp
+    var ScreenText = 40.sp
+    var ScreenPadding = 20.dp
+    var ScreenLineHeight = 40.sp
+    val  config = LocalConfiguration.current
+    if(config.orientation == Configuration.ORIENTATION_LANDSCAPE){
+        DisplayWeight = 3f
+        textPadding = 5.dp
+        ButtonText = 20.sp
+        ScreenPadding = 10.dp
+        ScreenText = 25.sp
+        ScreenLineHeight = 25.sp
+    }
+
     Box(modifier = Modifier.fillMaxSize().background(color = BackgroundBlack),
         contentAlignment = Alignment.BottomCenter){
         Column(modifier = Modifier.fillMaxWidth().padding(10.dp),
@@ -66,45 +85,45 @@ fun CalculatorUI(
         ) {
             Text(text = state.value , modifier = Modifier
                 .fillMaxWidth()
-                .padding(30.dp)
-                .weight(7f)
+                .padding(ScreenPadding)
+                .weight(DisplayWeight)
                 .fillMaxHeight()
                 , textAlign = TextAlign.End ,
                 fontWeight = FontWeight.Bold ,
                 maxLines = 10,
-                fontSize = 50.sp,
+                fontSize = ScreenText,
                 color = Color.White,
-                lineHeight = 40.sp)
+                lineHeight = ScreenLineHeight)
 
             Row (horizontalArrangement = Arrangement.spacedBy(15.dp) , modifier = Modifier.weight(1f)){
-                CalculatorButton(content = "CE", modifier = Modifier.weight(1f) , fontSize = 30.sp, backGroundColor = LightGray, onCLick = {onAction(CalculatorAction.Clear)})
-                CalculatorButton(content = "Del", modifier = Modifier.weight(1f), fontSize = 30.sp, backGroundColor = LightGray, onCLick = {onAction(CalculatorAction.Delete)})
-                CalculatorButton(content = "%", modifier = Modifier.weight(1f), fontSize = 30.sp, backGroundColor = LightGray, onCLick = {onAction(CalculatorAction.inputOperation(Operator.Modulus))})
-                CalculatorButton(content = "/", modifier = Modifier.weight(1f), fontSize = 30.sp, backGroundColor = LightGray, onCLick = {onAction(CalculatorAction.inputOperation(Operator.Divide))})
+                CalculatorButton(content = "CE", modifier = Modifier.weight(1f) , fontSize = ButtonText, textPadding = textPadding , backGroundColor = LightGray, onCLick = {onAction(CalculatorAction.Clear)})
+                CalculatorButton(content = "Del", modifier = Modifier.weight(1f), fontSize = ButtonText, textPadding = textPadding , backGroundColor = LightGray, onCLick = {onAction(CalculatorAction.Delete)})
+                CalculatorButton(content = "%", modifier = Modifier.weight(1f), fontSize = ButtonText, textPadding = textPadding , backGroundColor = LightGray, onCLick = {onAction(CalculatorAction.inputOperation(Operator.Modulus))})
+                CalculatorButton(content = "/", modifier = Modifier.weight(1f), fontSize = ButtonText, textPadding = textPadding , backGroundColor = LightGray, onCLick = {onAction(CalculatorAction.inputOperation(Operator.Divide))})
             }
             Row (horizontalArrangement = Arrangement.spacedBy(15.dp) , modifier = Modifier.weight(1f)){
-                CalculatorButton(content = "7", modifier = Modifier.weight(1f), fontSize = 30.sp, onCLick = {onAction(CalculatorAction.inputNumber(7))})
-                CalculatorButton(content = "8", modifier = Modifier.weight(1f), fontSize = 30.sp, onCLick = {onAction(CalculatorAction.inputNumber(8))})
-                CalculatorButton(content = "9", modifier = Modifier.weight(1f), fontSize = 30.sp, onCLick = {onAction(CalculatorAction.inputNumber(9))})
-                CalculatorButton(content = "*", modifier = Modifier.weight(1f), fontSize = 30.sp, backGroundColor = LightGray, onCLick = {onAction(CalculatorAction.inputOperation(Operator.Multiply))})
+                CalculatorButton(content = "7", modifier = Modifier.weight(1f), fontSize = ButtonText, textPadding = textPadding , onCLick = {onAction(CalculatorAction.inputNumber(7))})
+                CalculatorButton(content = "8", modifier = Modifier.weight(1f), fontSize = ButtonText, textPadding = textPadding , onCLick = {onAction(CalculatorAction.inputNumber(8))})
+                CalculatorButton(content = "9", modifier = Modifier.weight(1f), fontSize = ButtonText, textPadding = textPadding , onCLick = {onAction(CalculatorAction.inputNumber(9))})
+                CalculatorButton(content = "*", modifier = Modifier.weight(1f), fontSize = ButtonText, textPadding = textPadding , backGroundColor = LightGray, onCLick = {onAction(CalculatorAction.inputOperation(Operator.Multiply))})
             }
             Row (horizontalArrangement = Arrangement.spacedBy(15.dp) , modifier = Modifier.weight(1f)){
-                CalculatorButton(content = "4", modifier = Modifier.weight(1f), fontSize = 30.sp, onCLick = {onAction(CalculatorAction.inputNumber(4))})
-                CalculatorButton(content = "5", modifier = Modifier.weight(1f), fontSize = 30.sp, onCLick = {onAction(CalculatorAction.inputNumber(5))})
-                CalculatorButton(content = "6", modifier = Modifier.weight(1f), fontSize = 30.sp, onCLick = {onAction(CalculatorAction.inputNumber(6))})
-                CalculatorButton(content = "-", modifier = Modifier.weight(1f), fontSize = 30.sp, backGroundColor = LightGray, onCLick = {onAction(CalculatorAction.inputOperation(Operator.Subtract))})
+                CalculatorButton(content = "4", modifier = Modifier.weight(1f), fontSize = ButtonText, textPadding = textPadding , onCLick = {onAction(CalculatorAction.inputNumber(4))})
+                CalculatorButton(content = "5", modifier = Modifier.weight(1f), fontSize = ButtonText, textPadding = textPadding , onCLick = {onAction(CalculatorAction.inputNumber(5))})
+                CalculatorButton(content = "6", modifier = Modifier.weight(1f), fontSize = ButtonText, textPadding = textPadding , onCLick = {onAction(CalculatorAction.inputNumber(6))})
+                CalculatorButton(content = "-", modifier = Modifier.weight(1f), fontSize = ButtonText, textPadding = textPadding , backGroundColor = LightGray, onCLick = {onAction(CalculatorAction.inputOperation(Operator.Subtract))})
             }
             Row (horizontalArrangement = Arrangement.spacedBy(15.dp) , modifier = Modifier.weight(1f)){
-                CalculatorButton(content = "1", modifier = Modifier.weight(1f), fontSize = 30.sp, onCLick = {onAction(CalculatorAction.inputNumber(1))})
-                CalculatorButton(content = "2", modifier = Modifier.weight(1f), fontSize = 30.sp, onCLick = {onAction(CalculatorAction.inputNumber(2))})
-                CalculatorButton(content = "3", modifier = Modifier.weight(1f), fontSize = 30.sp, onCLick = {onAction(CalculatorAction.inputNumber(3))})
-                CalculatorButton(content = "+", modifier = Modifier.weight(1f), fontSize = 30.sp, backGroundColor = LightGray, onCLick = {onAction(CalculatorAction.inputOperation(Operator.Add))})
+                CalculatorButton(content = "1", modifier = Modifier.weight(1f), fontSize = ButtonText, textPadding = textPadding , onCLick = {onAction(CalculatorAction.inputNumber(1))})
+                CalculatorButton(content = "2", modifier = Modifier.weight(1f), fontSize = ButtonText, textPadding = textPadding , onCLick = {onAction(CalculatorAction.inputNumber(2))})
+                CalculatorButton(content = "3", modifier = Modifier.weight(1f), fontSize = ButtonText, textPadding = textPadding , onCLick = {onAction(CalculatorAction.inputNumber(3))})
+                CalculatorButton(content = "+", modifier = Modifier.weight(1f), fontSize = ButtonText, textPadding = textPadding , backGroundColor = LightGray, onCLick = {onAction(CalculatorAction.inputOperation(Operator.Add))})
             }
             Row (horizontalArrangement = Arrangement.spacedBy(15.dp) , modifier = Modifier.weight(1f)){
-                CalculatorButton(content = "^", modifier = Modifier.weight(1f), fontSize = 30.sp, onCLick = {onAction(CalculatorAction.inputOperation(Operator.Power))})
-                CalculatorButton(content = "0", modifier = Modifier.weight(1f), fontSize = 30.sp, onCLick = {onAction(CalculatorAction.inputNumber(0))})
-                CalculatorButton(content = ".", modifier = Modifier.weight(1f), fontSize = 30.sp, onCLick = {onAction(CalculatorAction.inputDecimal)})
-                CalculatorButton(content = "=", modifier = Modifier.weight(1f), fontSize = 30.sp , backGroundColor = DarkPurple, onCLick = {onAction(CalculatorAction.Answer)})
+                CalculatorButton(content = "^", modifier = Modifier.weight(1f), fontSize = ButtonText, textPadding = textPadding , onCLick = {onAction(CalculatorAction.inputOperation(Operator.Power))})
+                CalculatorButton(content = "0", modifier = Modifier.weight(1f), fontSize = ButtonText, textPadding = textPadding , onCLick = {onAction(CalculatorAction.inputNumber(0))})
+                CalculatorButton(content = ".", modifier = Modifier.weight(1f), fontSize = ButtonText, textPadding = textPadding , onCLick = {onAction(CalculatorAction.inputDecimal)})
+                CalculatorButton(content = "=", modifier = Modifier.weight(1f), fontSize = ButtonText, textPadding = textPadding , backGroundColor = DarkPurple, onCLick = {onAction(CalculatorAction.Answer)})
             }
         }
     }
